57481bd2f091e5b5af3d2512ac13e5497ff5a91c
AllHailMegatron <ahmadrthomp@gmail.com>
2019-08-26 18:23:27 -0400

    Add columns for cars table

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const server = require('./api/server.js');
 
 const port =process.env.PORT || 6000;
 server.listen(port, () => {
-  console.log(`\n*** Web API Listening on http://localhost:${port} ***\n`);
+  console.log(`\n***Listening on port http://localhost:${port} ***\n`);
 });
