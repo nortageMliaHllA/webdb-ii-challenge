43db859f879146acc5cc0bab5be0a737b0dca829
AllHailMegatron <ahmadrthomp@gmail.com>
2019-08-27 20:50:36 -0400

    Fix migration and created seed file

--- knexfile.js
+++ knexfile.js
@@ -1,17 +1,18 @@
 // Update with your config settings.
 
 module.exports = {
-
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './data/cars.db3'
+      filename: './data/cars.db3',
     },
     useNullAsDefault: true,
     migrations: {
-      directory: './data/migrations'
+      directory: './migrations'
+    },
+    seeds: {
+      directory: './seeds'
     }
   },
-
 
 };
